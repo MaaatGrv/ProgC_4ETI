4f6329ef50472ee51ede5b57d8dac6e9cc7628dc
TheotimeP <perricht@gmail.com>
2022-09-23 16:40:34 +0200

    comment

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,11 +1,9 @@
-//Print a triangle of 10 lines with the character * using a boucle for.
-
 #include <stdio.h>
 
 int main()
 {
     int compteur = 10;
-    if (compteur < 4)
+    if (compteur < 4) //si le compteur est plus petit que 4
     {
         printf(" %d < 4 inacceptable", compteur);
     }
@@ -15,11 +13,11 @@ int main()
         {
             for (int j = 0; j < i; j++)
             {
-                if (j == 0 | j == i-1 | i == compteur)
+                if (j == 0 | j == i-1 | i == compteur) //on place une étoile * au début et a la fin de chaque ligne, et on n'en met que sur la derniere ligne
                 {
                     printf("*");
                 }
-                else
+                else //sinon on met des #
                 {
                     printf("#");
                 }
