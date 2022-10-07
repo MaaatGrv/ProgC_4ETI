9c0c24bdc4c748dd3db675d90d30979732942483
TheotimeP <perricht@gmail.com>
2022-10-07 13:37:28 +0200

    new

---
+++ TP3/src/chercher.c
@@ -0,0 +1,28 @@
+//create a tab of 100 random numbers and find a number in it, if it present print the message "entier present"
+
+#include <stdio.h>
+
+int main(void)
+{
+    int tab[100];
+    int i;
+    int entier;
+    int present = 0;
+
+    for (i = 0; i < 100; i++)
+    {
+        tab[i] = rand() % 100;
+
+    }
+
+    printf(" entier = ");
+    scanf("%d", &entier);
+    for (i = 0; i < 100; i++)
+    {
+        if (tab[i] == entier)
+        {
+        printf("entier present");
+        }
+    }
+    return 0;
+}
