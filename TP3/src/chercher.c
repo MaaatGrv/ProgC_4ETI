754771d3bb97057068ff3a062508f8d00692d602
TheotimeP <perricht@gmail.com>
2022-10-07 16:56:46 +0200

    finiiiiiiii

--- TP3/src/chercher.c
+++ TP3/src/chercher.c
@@ -1,4 +1,4 @@
-//create a tab of 100 random numbers and find a number in it, if it present print the message "entier present"
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,42 +29,33 @@ int Exo_3_4()
     return 0;
 }
 
-int main(){
-
-    int i=0;
-    int verif=1;
-    char phrase_seach[200];
-
-    //creation du tableau de phrases
-    char *tableau[10] = {"J'aime beurrer mes biscottes\n","Je vole Jack\n","Il n'y a pas de bonne ou de mauvaise situation\n","j'ai tout fini chef\n","Je suis vengeance\n","Konnichiwa\n","Il est mignon\n","Faut donner sa chance au produit\n","Il fait au plus 2m\n","Un petit pokemon\n"};
-    //on demande à l'utilisateur la phrase qu'il cherche
-    
-    
-    //definition des pointeurs
-    char *ptrtableau = 0;
-    char *ptrphrase = &phrase_seach[0];
- 
-    for (i=0;i<10;i++){  //on parcourt toutes les phrases du tableau
-        ptrtableau=tableau[i]; //on place le pointeur sur le premier caractere de la phrase
-        verif=1;
-        ptrphrase = &phrase_seach[0];
-        while ((*ptrtableau!=0) && (*ptrphrase!=0)){  //on parcourt les phrases jusqu'à ce que l'une soit finie
-            if(*ptrtableau!=*ptrphrase){  //Si les deux lettres sont differentes
-                verif=0; //les phrases sont differentes
-            }
-            ptrtableau++; //on passe à la lettre suivante
-            ptrphrase++; //on passe à la lettre suivante
-            
-        }
-        if ((*ptrtableau!=0) || (*ptrphrase!=0)){  //Si une des phrases se finit avant l'autre
-            verif=0;  //les phrases sont differentes
-        }
-        if (verif==1){  //la phrase est identique a celle testee
-            printf("La phrase est dans la liste\n");
-            return (0);  //On arrete le programme si on a trouvé la phrase
-        }
+int Exo_3_8() {
+    char *sentences[10];
+    sentences[0] = "J'aime beurrer mes biscottes";
+    sentences[1] = "Il n'y a pas de bonne ou de mauvaise situation";
+    sentences[2] = "j'ai tout fini chef";
+    sentences[3] = "je suis vengeance";
+    sentences[4] = "konichiwa";
+    sentences[5] = "il est mignon ";
+    sentences[6] = "Faut donner sa chance au produit";
+    sentences[7] = "Il fait au plus 2m";
+    sentences[8] = "l'equitation est un sport complexe";
+    sentences[9] = "Je suis un petit pokemon";
+    char *sentence = "je suis vengeance";
+    int i = 0;
+    while (i < 10 && sentences[i] != sentence) {
+        i++;
     }
-    printf("La phrase n'est pas dans la liste\n");     //Le programme n'a pas été arrêté donc la phrase n'a pas été trouvée
-    
+    if (i < 10) {
+        printf("la phrase est dans le tableau");
+    } else {
+        printf("la phrase n'est pas dans le tableau");
+    }
+    return 0;
+}
+
+int main() {
+
+    Exo_3_8();
     return 0;
 }
