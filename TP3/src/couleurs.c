71781146b652eacb2c2f93290d88cfadc9cee288
Mathis Gorvien <mathis.gorvien@gmail.com>
2022-10-09 16:05:39 +0200

    ok

--- TP3/src/couleurs.c
+++ TP3/src/couleurs.c
@@ -1,47 +1,44 @@
+//create a tab of 100 random numbers and find a number in it, if it present print the message "entier present"
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
-typedef struct {
-    unsigned char r;
-    unsigned char g;
-    unsigned char b;
-    unsigned char a;
-} Couleur;
-
-typedef struct {
-    unsigned char r;
-    unsigned char g;
-    unsigned char b;
-    unsigned char a;
-    int count;
-} CouleurCount;
 
 int main(){
-    srand(time(NULL));
-    Couleur couleurs[1000];
-    CouleurCount couleursCount[1000];
-    for (int i = 0; i < 999; i++) {
-        couleurs[i].r = rand() % 3;
-        couleurs[i].g = rand() % 3;
-        couleurs[i].b = rand() % 3;
-        couleurs[i].a = rand() % 3;
-    }
-    for (int i = 0; i < 999; i++)
-    {
-        couleursCount[i].r = couleurs[i].r;
-        couleursCount[i].g = couleurs[i].g;
-        couleursCount[i].b = couleurs[i].b;
-        couleursCount[i].a = couleurs[i].a;
-        couleursCount[i].count = 1;
-        for (int j = 0; j < 999; j++)
-        {
-            if (couleurs[i].r == couleurs[j].r && couleurs[i].g == couleurs[j].g && couleurs[i].b == couleurs[j].b && couleurs[i].a == couleurs[j].a && i != j)
-            {
-                couleursCount[i].count++;
+
+    int i=0;
+    int verif=1;
+    char phrase_seach[200];
+
+    //creation du tableau de phrases
+    char *tableau[10] = {"J'aime beurrer mes biscottes\n","Je vole Jack\n","Il n'y a pas de bonne ou de mauvaise situation\n","j'ai tout fini chef\n","Je suis vengeance\n","Konnichiwa\n","Il est mignon\n","Faut donner sa chance au produit\n","Il fait au plus 2m\n","Un petit pokemon\n"};
+    //on demande à l'utilisateur la phrase qu'il cherche
+    
+    
+    //definition des pointeurs
+    char *ptrtableau = 0;
+    char *ptrphrase = &phrase_seach[0];
+ 
+    for (i=0;i<10;i++){  //on parcourt toutes les phrases du tableau
+        ptrtableau=tableau[i]; //on place le pointeur sur le premier caractere de la phrase
+        verif=1;
+        ptrphrase = &phrase_seach[0];
+        while ((*ptrtableau!=0) && (*ptrphrase!=0)){  //on parcourt les phrases jusqu'à ce que l'une soit finie
+            if(*ptrtableau!=*ptrphrase){  //Si les deux lettres sont differentes
+                verif=0; //les phrases sont differentes
             }
+            ptrtableau++; //on passe à la lettre suivante
+            ptrphrase++; //on passe à la lettre suivante
+            
         }
-        printf("Couleur %d : r=0x%02x, g=0x%02x, b=0x%02x, a=0x%02x, count=%d \n", i + 1, couleursCount[i].r, couleursCount[i].g, couleursCount[i].b, couleursCount[i].a, couleursCount[i].count);
+        if ((*ptrtableau!=0) || (*ptrphrase!=0)){  //Si une des phrases se finit avant l'autre
+            verif=0;  //les phrases sont differentes
+        }
+        if (verif==1){  //la phrase est identique a celle testee
+            printf("La phrase est dans la liste\n");
+            return (0);  //On arrete le programme si on a trouvé la phrase
+        }
     }
+    printf("La phrase n'est pas dans la liste\n");     //Le programme n'a pas été arrêté donc la phrase n'a pas été trouvée
+    
     return 0;
 }
