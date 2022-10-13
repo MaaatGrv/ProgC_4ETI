9b2ab6950b651f7a1a4135a6d6c85ca1d551fcd7
Mathis Gorvien <mathis.gorvien@gmail.com>
2022-10-13 14:17:05 +0200

    add comments tp3

--- TP3/src/octets.c
+++ TP3/src/octets.c
@@ -4,19 +4,23 @@
 
 int main(void)
 {
-    int a = 1893; // déclaration d'un entier
+    // déclaration des variables
+    int a = 1893; 
     short b = 1893;
     float c = 1893.0;
     long int d = 1893;
     double e = 1893.0;
     long double f = 1893.0;
 
+    // Déclaration des pointeurs
     char* p = (char*)&a;
     char*s = (char*)&b;
     char*t = (char*)&c;
     char*u = (char*)&d;
     char*v = (char*)&e;
     char*w = (char*)&f;
+
+    // Affichage des valeurs des pointeurs
     printf("octets int = %x %x %x %x \n", *(p+3), *(p + 2), *(p + 1), *p); // affichage des octets de a en respectant la convention little endian
     printf("octets short = %x %x \n", *(s + 1), *s);
     printf("octets  float= %x %x %x %x \n", *(t + 3), *(t + 2), *(t + 1), *t);
--- TP3/src/recherche_dichotomique.c
+++ TP3/src/recherche_dichotomique.c
@@ -10,17 +10,17 @@ int main(void)
     int j;
     int temp;
     int entier;
-    for (i = 0; i < 100; i++)
+    for (i = 0; i < 100; i++) // création du tableau de 100 entiers aléatoires
     {
         tab[i] = rand() % 100;
     }
-    for (i = 0; i < 100; i++) //tableau trié en ordre croissant
+    for (i = 0; i < 100; i++) // Le tableau est trié dans l'ordre croissant
     {
-        for (j = 0; j < 100; j++)
+        for (j = 0; j < 100; j++) 
         {
             if (tab[i] < tab[j])
             {
-                temp = tab[i];
+                temp = tab[i]; // variable temporaire pour stocker la valeur de tab[i]
                 tab[i] = tab[j];
                 tab[j] = temp;
             }
@@ -30,15 +30,27 @@ int main(void)
     printf(" sorted = "); 
     for (i = 0; i < 100; i++)
     {
-        printf("%d ", tab[i]);
+        printf("%d ", tab[i]); // Affichage du tableau trié
     } 
 
-//on cherche un entier par dichotomie
+    // on cherche un entier par dichotomie
     printf(" entier = ");
-    scanf("%d", &entier);
+    scanf("%d", &entier); // lecture de l'entier à chercher
     int min = 0;
     int max = sizeof(tab)/sizeof(tab[0]); //on divise la taille totale du tableau par la taille de l’élément du tableau pour obtenir le nombre d’éléments du tableau
-    int milieu = (min + max) / 2;
+    int milieu = (min + max) / 2; // on calcule le milieu du tableau
+    
+    /*
+    tant que l'entier n'est pas trouvé et que le tableau n'est pas vide
+    on cherche l'entier dans la partie du tableau où il peut se trouver
+    on réduit la taille du tableau à chaque itération
+    on calcule le milieu du tableau
+    on compare l'entier à chercher avec l'entier du milieu du tableau
+    si l'entier est inférieur à l'entier du milieu du tableau
+    on cherche l'entier dans la partie gauche du tableau
+    sinon on cherche l'entier dans la partie droite du tableau
+    */
+
     while (tab[milieu] != entier && min < max)
     {
         if (tab[milieu] < entier)
