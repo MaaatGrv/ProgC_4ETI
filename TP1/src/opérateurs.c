ef179acb2133db7375dfb99b67fac2e52ef208eb
Mathis Gorvien <mathis.gorvien@gmail.com>
2022-10-13 13:36:13 +0200

    add comments TP1

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,11 +1,11 @@
-
-
 #include <stdio.h>
 
 int main()
 {
+    // Initialisation des variables
     int a = 16; 
     int b = 3;
+    // On test et affiche les diffénrentes opérations
     printf("a+b=%d, a-b=%d, a*b=%d, a/b=%d, a%%b=%d, a&&b=%d, a||b=%d, !a=%d", a+b, a-b, a*b, a/b, a%b, a&&b, a||b, !a);
     return 0;
 }
--- TP1/src/variables.c
+++ TP1/src/variables.c
@@ -3,6 +3,7 @@
 
 int main()
 {
+    // On attribue une valeurs pour les différents types de variables
     char v = 5;
     signed char v1 = 5;
     unsigned char v2 = 5;
@@ -21,6 +22,7 @@ int main()
     float v15 = 5;
     double v16 = 5;
     long double v17 = 5;
+    // On affiche les valeurs des variables avec leur type
     printf("char: %d, signed char: %d, unsigned char: %d, short: %d, signed short: %d, unsigned short: %d, int: %d, signed int: %d, unsigned int: %d, long: %d, signed long: %d, unsigned long: %d, long long: %d, signed long long: %d, unsigned long long: %d, float: %d, double: %d, long double: %d", v, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17);
     return 0;
 }
