607c6e8874143b97c1654159d677e846ab5b404d
TheotimeP <perricht@gmail.com>
2022-10-14 15:05:56 +0200

    new

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
+#include "operator.h"
+
+int main(){
+    int num1 = 16;
+    int num2 = 3;
+    char op = '+'; //choix de l'opérateur
+    switch (op) //choix de l'opérateur
+    {
+        case '+': //addition
+            printf("%d", somme(num1,num2));
+            break;
+        case '-': //soustraction
+            printf("%d", difference(num1,num2));
+            break;
+        case '*': //multiplication
+            printf("%d", multiplication(num1,num2));
+            break;
+        case '/': //division
+            printf("%d", quotient(num1,num2));
+            break;
+        case '%': //modulo
+            printf("%d", modulo(num1,num2));
+            break;
+        case '&': //et logique
+            printf("%d", et(num1,num2));
+            break;
+        case '|': //ou logique
+            printf("%d", ou(num1,num2));
+            break;
+        case '~': //non logique
+            printf("%d", negation(num1));
+            break;
+        default: //si l'opérateur n'est pas reconnu
+            printf("Error");
+            break;
+    }
+    return 0;
+}
+
 
---
+++ TP4/src/operator.c
@@ -0,0 +1,34 @@
+#include "operator.h"
+#include <stdio.h>
+
+int somme(int a,int b){
+    return a+b;
+}
+
+int difference(int a,int b){
+    return a-b;
+}
+
+int multiplication(int a,int b){
+    return a*b;
+}
+
+int quotient(int a,int b){
+    return a/b;
+}
+
+int modulo(int a,int b) {
+    return a%b;
+}
+
+int et(int a,int b) {
+    return a&b;
+}
+
+int ou(int a,int b) {
+    return a|b;
+}
+
+int negation(int a) {
+    return ~a;
+}
